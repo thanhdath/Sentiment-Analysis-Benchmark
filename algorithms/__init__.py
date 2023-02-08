dfd4793e2616b226e48ebb595d07d4148d144020
thanhdat285 <thanhdath97@gmail.com>
2023-02-09 00:06:49 +0700

    Add trainer into LMForSequenceClassification

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,7 +1,7 @@
 from .linear_svm_tfidf import LinearSVMTFIDF
 from .linear_svm_word2vec import LinearSVMWord2Vec
 from .logistic_regressor_word2vec import LogisticRegressorWord2Vec
-from .bert import DocumentEmbedding
+from .bert import LMForSequenceClassification
 from .naive_bayes import NaiveBayes
 from .random_forest import RandomForest
 
@@ -14,7 +14,26 @@ def init_model(model_name):
     elif model_name == "logistic_regressor_word2vec":
         model = LogisticRegressorWord2Vec(f"models/{model_name}.pkl")
     elif model_name in ["distilbert-base-uncased", "bert-base-uncased", "roberta-base"]:
-        model = DocumentEmbedding(model_name)
+        model = LMForSequenceClassification(model_name)
+    elif model_name == "naive_bayes":
+        model = NaiveBayes(f"models/{model_name}.pkl")
+    elif model_name == "random_forest":
+        model = RandomForest(f"models/{model_name}.pkl")
+    else:
+        raise NotImplementedError()
+
+    return model
+
+
+def init_trainer(model_name):
+    if model_name == "linear_svm_tfidf":
+        model = LinearSVMTFIDF(f"models/{model_name}.pkl")
+    elif model_name == "linear_svm_word2vec":
+        model = LinearSVMWord2Vec(f"models/{model_name}.pkl")
+    elif model_name == "logistic_regressor_word2vec":
+        model = LogisticRegressorWord2Vec(f"models/{model_name}.pkl")
+    elif model_name in ["distilbert-base-uncased", "bert-base-uncased", "roberta-base"]:
+        model = LMForSequenceClassification(model_name, load_inference_model=False)
     elif model_name == "naive_bayes":
         model = NaiveBayes(f"models/{model_name}.pkl")
     elif model_name == "random_forest":
--- algorithms/bert.py
+++ algorithms/bert.py
@@ -1,30 +1,142 @@
 from transformers import AutoTokenizer
 import torch
 from transformers import AutoModelForSequenceClassification
+import numpy as np
+from transformers import AutoTokenizer
+import torch
+from datasets import Dataset
+from transformers import DataCollatorWithPadding
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+import time
+from sklearn.metrics import (
+    classification_report,
+    f1_score,
+    accuracy_score,
+    precision_score,
+    recall_score,
+)
 
-class DocumentEmbedding:
-    def __init__(self, model_name):
-        model_name = f"models/{model_name}"
-        
+
+class LMForSequenceClassification:
+    def __init__(self, model_name, load_inference_model=True):
+        if load_inference_model:
+            # Load pretrained model for inference
+            pretrained_model_path = f"models/{model_name}"
+        else:
+            # Initialize from original model, finetuning with function train()
+            pretrained_model_path = model_name
+
         if "uncased" in model_name:
             self.lower = True
         else:
             self.lower = False
-            
+
+        self.model_name = model_name
         self.model = AutoModelForSequenceClassification.from_pretrained(
-            model_name, num_labels=3
+            pretrained_model_path, num_labels=3
         )
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        
+        self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model_path)
+        self.data_collator = DataCollatorWithPadding(tokenizer=self.tokenizer)
+        self.load_inference_model = load_inference_model
+
         self.id2label = {0: "negative", 1: "neutral", 2: "positive"}
-        
-    def predict(self, sentence):
+
+    def predict(self, sentence, output_dir):
         if self.lower:
             sentence = sentence.lower()
-            
-        tokenized_sentence = self.tokenizer(sentence, truncation=True, return_tensors="pt")
+
+        tokenized_sentence = self.tokenizer(
+            sentence, truncation=True, return_tensors="pt"
+        )
         with torch.no_grad():
             logits = self.model(**tokenized_sentence).logits[0]
         label = logits.argmax(axis=-1)
         return self.id2label[int(label)]
-        
+
+    def preprocess_function(self, examples):
+        return self.tokenizer(examples["text"], truncation=True)
+
+    def train(self, train_data, device=None, output_model_name=None):
+        """
+        train_data: list of [{'text', 'label_number'}]
+        device: e.g. cuda:0, cpu, cuda:1
+        """
+
+        train_data = Dataset.from_list(train_data)
+
+        if device is None:
+            device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+        tokenized_train_data = train_data.map(self.preprocess_function, batched=True)
+
+        model = self.model.to(device)
+
+        training_args = TrainingArguments(
+            output_dir="./results",
+            learning_rate=2e-5,
+            per_device_train_batch_size=16,
+            per_device_eval_batch_size=1,
+            num_train_epochs=5,
+            weight_decay=0.01,
+        )
+
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=tokenized_train_data,
+            tokenizer=self.tokenizer,
+            data_collator=self.data_collator,
+        )
+
+        trainer.train()
+
+        if output_model_name is None:
+            output_model_name = f"{self.model_name}-{time.time()}"
+
+        output_model_path = f"models/{output_model_name}"
+        trainer.save_model(output_model_path)
+
+        print(f"Model has been saved to {output_model_path}")
+
+    def evaluate(self, test_data):
+        test_data = Dataset.from_list(test_data)
+        tokenized_test_data = test_data.map(self.preprocess_function, batched=True)
+
+        training_args = TrainingArguments(
+            output_dir="./results", per_device_eval_batch_size=2, num_train_epochs=0
+        )
+
+        trainer = Trainer(
+            model=self.model,
+            args=training_args,
+            tokenizer=self.tokenizer,
+            data_collator=self.data_collator,
+        )
+
+        result = trainer.predict(tokenized_test_data)
+        result = result.predictions.argmax(axis=1)
+
+        y_test = [x["label"] for x in tokenized_test_data]
+
+        acc = accuracy_score(y_test, result)
+        print(f"Accuracy: micro: {acc:.3f}")
+
+        p_micro = precision_score(y_test, result, average="micro")
+        p_macro = precision_score(y_test, result, average="macro")
+        print(f"Precision: micro-macro: {p_micro:.3f}-{p_macro:.3f}")
+
+        r_micro = recall_score(y_test, result, average="micro")
+        r_macro = recall_score(y_test, result, average="macro")
+        print(f"Recall: micro-macro: {r_micro:.3f}-{r_macro:.3f}")
+
+        f1_micro = f1_score(y_test, result, average="micro")
+        f1_macro = f1_score(y_test, result, average="macro")
+        print(f"F1: micro-macro: {f1_micro:.3f}-{f1_macro:.3f}")
+
+        print(
+            classification_report(
+                y_test,
+                result,
+                target_names=[self.id2label[x] for x in range(len(self.id2label))],
+            )
+        )
